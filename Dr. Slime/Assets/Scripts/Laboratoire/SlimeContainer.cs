24205bff7c25eb4a3fd0d375f4b9839d931aa592
Th4umas <147378335+Th4umas@users.noreply.github.com>
2025-02-22 17:07:00 +0100

    re

--- Dr. Slime/Assets/Scripts/Laboratoire/SlimeContainer.cs
+++ Dr. Slime/Assets/Scripts/Laboratoire/SlimeContainer.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SlimeContainer : MonoBehaviour
 {
@@ -7,6 +9,10 @@ public class SlimeContainer : MonoBehaviour
     private int slimeCount = 0;
     private string slimeType = "None";
 
+    public List<int> fluids = new List<int>();
+
+    public GameObject finished;
+
     private void Start()
     {
         m_MeshRenderer = GetComponent<MeshRenderer>();
@@ -14,10 +20,10 @@ public class SlimeContainer : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (LaboSlimes.selectedSlime != null) // Check if a slime is selected
+        if (LaboSlimes.selectedSlime != null)
         {
             AddSlime(LaboSlimes.selectedSlime.gameObject);
-            LaboSlimes.selectedSlime.Deselect(); // Deselect the slime after adding it
+            LaboSlimes.selectedSlime.Deselect();
         }
     }
 
@@ -28,14 +34,17 @@ public class SlimeContainer : MonoBehaviour
         if (slime.GetComponent<LaboCanna>() != null)
         {
             slimeType = "CannaContainer";
+            fluids.Add(1);
         }
         else if (slime.GetComponent<LaboChampi>() != null)
         {
             slimeType = "ChampiContainer";
+            fluids.Add(2);
         }
         else if (slime.GetComponent<LaboCrack>() != null)
         {
             slimeType = "CrackContainer";
+            fluids.Add(3);
         }
 
         Debug.Log("Slime added! Total count: " + slimeCount + " | Last Slime Type: " + slimeType);
@@ -43,9 +52,27 @@ public class SlimeContainer : MonoBehaviour
 
     private void Update()
     {
-        if(slimeCount > 0)
+        if(slimeCount ==1)
         {
             m_MeshRenderer.material.color = Color.blue;
         }
+
+        if(slimeCount == 3)
+        {
+            //fluidcheck(fluids);
+            m_MeshRenderer.material.color = Color.red;
+            Instantiate(finished, transform.position, Quaternion.identity);
+        }
     }
+
+    //when the container has three ingredients, it either blows up or becomes something sellable
+    /*
+    public bool fluidcheck(List l)
+    {
+        for (int i = 0) ;
+        {
+            
+        }
+        return false;
+    }*/
 }
