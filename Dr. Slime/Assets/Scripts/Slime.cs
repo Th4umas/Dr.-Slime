2b0fa04048c521ef09cd3df8d2d86e278d0305f0
Th4umas <147378335+Th4umas@users.noreply.github.com>
2025-02-22 03:36:18 +0100

    update

--- Dr. Slime/Assets/Scripts/Slime.cs
+++ Dr. Slime/Assets/Scripts/Slime.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class Slime : MonoBehaviour
 {
-    public float waitTime = 3f;
-    public float moveTime = 2f;
-    public float moveSpeed = 2f;
+    public float waitTimemin = 3f;
+    public float waitTimemax = 5f;
+    public float moveTimeMin = 1f; 
+    public float moveTimeMax = 3f; 
+    public float moveSpeedMin = 1f;
+    public float moveSpeedMax = 3f;
 
     void Start()
     {
@@ -14,9 +17,11 @@ public class Slime : MonoBehaviour
 
     private IEnumerator MovementLoop()
     {
+        yield return new WaitForSeconds(Random.Range(0f, 3f));
+
         while (true)
         {
-            yield return new WaitForSeconds(waitTime);
+            yield return new WaitForSeconds(Random.Range(waitTimemin, waitTimemax));
 
             StartCoroutine(MoveRandomly());
         }
@@ -25,11 +30,14 @@ public class Slime : MonoBehaviour
     private IEnumerator MoveRandomly()
     {
         Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
+        float randomMoveTime = Random.Range(moveTimeMin, moveTimeMax); 
+        float randomSpeed = Random.Range(moveSpeedMin, moveSpeedMax);  
+
         float startTime = Time.time;
 
-        while (Time.time < startTime + moveTime)
+        while (Time.time < startTime + randomMoveTime)
         {
-            transform.position += randomDirection * moveSpeed * Time.deltaTime;
+            transform.position += randomDirection * randomSpeed * Time.deltaTime;
             yield return null;
         }
     }
--- Dr. Slime/Assets/Scripts/SlimeSpawner.cs
+++ Dr. Slime/Assets/Scripts/SlimeSpawner.cs
@@ -6,36 +6,24 @@ public class SlimeSpawner : MonoBehaviour
 {
     public Slime slimePrefab;
 
-    public int max = 25;
-    public int min = 15;
+    public int maxCanna = 25;
+    public int minCanna = 15;
 
     private Vector3 pos;
-    private float randomx;
-    private float randomz;
 
     void Start()
     {
-
-
         spawn();
-    }
-
-    void Update()
-    {
-        
     }
 
     private void spawn()
     {
-        int randomSpawn = Random.Range(min, max);
+        int randomSpawn = Random.Range(minCanna, maxCanna);
 
         while (randomSpawn > 0)
         {
             randomSpawn--;
-            randomx = Random.Range(-10, 10);
-            randomz = Random.Range(-10, 10);
-
-            pos = new Vector3(randomx, 0.5f, randomz);
+            pos = new Vector3(Random.Range(-10, 10), 0.5f, Random.Range(-10, 10));
             Instantiate(slimePrefab, pos, Quaternion.identity);
         }
 
